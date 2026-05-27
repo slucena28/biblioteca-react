5ffb8008078661027c079569576ce568a710cf51
slucena28 <113914867+slucena28@users.noreply.github.com>
2026-05-27 12:57:42 -0300

    Create bookform.tsx

---
+++ src/components/bookform.tsx
@@ -0,0 +1,89 @@
+import {
+  FormEvent,
+  useState,
+} from "react";
+
+import { Book } from "../types/Book";
+
+interface BookFormProps {
+  onAddBook: (
+    book: Book
+  ) => Promise<void>;
+}
+
+export function BookForm({
+  onAddBook,
+}: BookFormProps) {
+  const [title, setTitle] =
+    useState<string>("");
+
+  const [author, setAuthor] =
+    useState<string>("");
+
+  const [status, setStatus] = useState<
+    "Lido" | "Não lido"
+  >("Não lido");
+
+  async function handleSubmit(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!title || !author) return;
+
+    await onAddBook({
+      title,
+      author,
+      status,
+    });
+
+    setTitle("");
+    setAuthor("");
+    setStatus("Não lido");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Título"
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)
+        }
+      />
+
+      <input
+        type="text"
+        placeholder="Autor"
+        value={author}
+        onChange={(e) =>
+          setAuthor(e.target.value)
+        }
+      />
+
+      <select
+        value={status}
+        onChange={(e) =>
+          setStatus(
+            e.target.value as
+              | "Lido"
+              | "Não lido"
+          )
+        }
+      >
+        <option value="Lido">
+          Lido
+        </option>
+
+        <option value="Não lido">
+          Não lido
+        </option>
+      </select>
+
+      <button type="submit">
+        Adicionar
+      </button>
+    </form>
+  );
+}
