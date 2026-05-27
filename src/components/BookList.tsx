5c1ec7286ae71b5c1a4c4956391c2b0752a96029
slucena28 <113914867+slucena28@users.noreply.github.com>
2026-05-27 12:58:34 -0300

    Create BookList.tsx

---
+++ src/components/BookList.tsx
@@ -0,0 +1,28 @@
+import { Book } from "../types/Book";
+
+import { BookItem } from "./BookItem";
+
+interface BookListProps {
+  books: Book[];
+
+  onDelete: (
+    id: string
+  ) => Promise<void>;
+}
+
+export function BookList({
+  books,
+  onDelete,
+}: BookListProps) {
+  return (
+    <ul>
+      {books.map((book) => (
+        <BookItem
+          key={book._id}
+          book={book}
+          onDelete={onDelete}
+        />
+      ))}
+    </ul>
+  );
+}
